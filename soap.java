034d5dbd8323499fa35dad6449beca7f460e53af
FAZIL AHAMMED.N <130854953+Fazil0018@users.noreply.github.com>
2025-03-22 22:49:11 +0530

    Create soap.java

---
+++ soap.java
@@ -0,0 +1,18 @@
+import java.util;
+class bile
+{
+  void soap(int money)
+  {
+    System.out.println("soap parchased :"+money);
+  }
+  void chocolate(int money)
+  {
+    System.out.println("chocolate oarchased :"+ money);
+  }
+  public static void main(String[] args) 
+  {
+    bile sc = new bile();
+    sc.soap(12);
+    sc.chocolate(15);  
+  }
+}

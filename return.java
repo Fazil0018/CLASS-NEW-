01e9d05bb974d6538660a2dda1939606913d4a83
FAZIL AHAMMED.N <130854953+Fazil0018@users.noreply.github.com>
2025-03-22 23:40:46 +0530

    Create return.java

---
+++ return.java
@@ -0,0 +1,15 @@
+import java.util;
+class bile
+{
+   int sum(int a,int b)
+   {
+    int c=a+b;
+    return c;
+   }
+   public static void main(String[] args)
+   {
+      bile sc = new bile();
+      int sum = sc.sum(10, 20);
+      System.out.println(sum); 
+   }
+}

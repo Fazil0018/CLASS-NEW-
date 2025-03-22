cd2c1fc8442013e69a7773ed7cb129d0ada8800c
FAZIL AHAMMED.N <130854953+Fazil0018@users.noreply.github.com>
2025-03-22 22:23:52 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,17 @@
+import java.util;
+class bile
+{
+  int a = 10;
+  int b = 20;
+
+  void sum()
+  {
+    System.out.println(b*a);
+  }
+
+  public static void main(String[] args)
+  {
+    bile sc = new bile();
+    sc.sum();
+  }
+}
